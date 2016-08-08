724844170547ded90994aa1ccf8de5cdca3100fd
Dale Fugier <dale@mcneel.com>
2016-08-08 10:14:30 -0700

    Added SampleCsInvertSelected.cs

---
+++ SampleCsCommands/SampleCsInvertSelected.cs
@@ -0,0 +1,34 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.DocObjects;
+
+namespace SampleCsCommands
+{
+  public class SampleCsInvertSelected : Command
+  {
+    public override string EnglishName => "SampleCsInverteSelected";
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      var filter = new ObjectEnumeratorSettings
+      {
+        NormalObjects = true,
+        LockedObjects = false,
+        HiddenObjects = false,
+        ActiveObjects = true,
+        ReferenceObjects = true
+      };
+
+      var rh_objects = doc.Objects.FindByFilter(filter);
+      foreach (var rh_obj in rh_objects)
+      {
+        var select = 0 == rh_obj.IsSelected(false) && rh_obj.IsSelectable();
+        rh_obj.Select(select);
+      }
+
+      doc.Views.Redraw();
+
+      return Result.Success;
+    }
+  }
+}
