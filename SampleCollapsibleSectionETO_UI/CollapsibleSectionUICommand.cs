9f54e09c3b35928fa4536b6a36a50f2bf950a5e6
Max Söderström <max.soderstrom@mcneel.com>
2016-09-29 14:09:21 +0300

    CollapsibleEtoSection -> EtoCollapsibleSection

--- SampleCollapsibleSectionETO_UI/CollapsibleSectionUICommand.cs
+++ SampleCollapsibleSectionETO_UI/CollapsibleSectionUICommand.cs
@@ -8,41 +8,41 @@ using Rhino.Input.Custom;
 
 namespace Project
 {
-    public class CollapsibleSectionUICommand : Command
+  public class CollapsibleSectionUICommand : Command
+  {
+    public CollapsibleSectionUICommand()
     {
-        public CollapsibleSectionUICommand()
-        {
-            // Rhino only creates one instance of each command class defined in a
-            // plug-in, so it is safe to store a refence in a static property.
-            Instance = this;
-        }
-
-        ///<summary>The only instance of this command.</summary>
-        public static CollapsibleSectionUICommand Instance
-        {
-            get; private set;
-        }
-
-        ///<returns>The command name as it appears on the Rhino command line.</returns>
-        public override string EnglishName
-        {
-            get { return "CollapsibleSectionUIExample"; }
-        }
-
-        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
-        {
-            // TODO: start here modifying the behaviour of your command.
-            // ---
-            System.Type panelType = typeof(CollapsibleSectionUIPanel);
-            Rhino.UI.Panels.RegisterPanel(CollapsibleSectionUIPlugIn.Instance, panelType, "CollapsibleSectionUIExample", null);
-
-            System.Guid panelId = CollapsibleSectionUIPanel.PanelId;
-            bool bVisible = Rhino.UI.Panels.IsPanelVisible(panelId);
-
-            if (!bVisible)
-                Rhino.UI.Panels.OpenPanel(panelId);
-
-            return Result.Success;
-        }
+      // Rhino only creates one instance of each command class defined in a
+      // plug-in, so it is safe to store a refence in a static property.
+      Instance = this;
     }
+
+    ///<summary>The only instance of this command.</summary>
+    public static CollapsibleSectionUICommand Instance
+    {
+      get; private set;
+    }
+
+    ///<returns>The command name as it appears on the Rhino command line.</returns>
+    public override string EnglishName
+    {
+      get { return "CollapsibleSectionUIExample"; }
+    }
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      // TODO: start here modifying the behaviour of your command.
+      // ---
+      System.Type panelType = typeof(CollapsibleSectionUIPanel);
+      Rhino.UI.Panels.RegisterPanel(CollapsibleSectionUIPlugIn.Instance, panelType, "CollapsibleSectionUIExample", null);
+
+      System.Guid panelId = CollapsibleSectionUIPanel.PanelId;
+      bool bVisible = Rhino.UI.Panels.IsPanelVisible(panelId);
+
+      if (!bVisible)
+        Rhino.UI.Panels.OpenPanel(panelId);
+
+      return Result.Success;
+    }
+  }
 }
--- SampleCollapsibleSectionETO_UI/SectionOne.cs
+++ SampleCollapsibleSectionETO_UI/SectionOne.cs
@@ -1,134 +1,131 @@
 ﻿using System;
-using Rhino;
-using Rhino.Commands;
 using Eto.Forms;
+using Rhino.UI;
 
 namespace Project
-{    
-     ///<summary>
-     /// The UI implementation of of Section one
-     ///</summary>
-    public class SectionOne : Section
+{
+  ///<summary>
+  /// The UI implementation of of Section one
+  ///</summary>
+  public class SectionOne : Section
+  {
+    private LocalizeStringPair m_caption;
+    private Button m_button;
+    private CheckBox m_checkbox;
+    private Label m_button_lb;
+    private Label m_checkbox_lb;
+    private Label m_button_clicks_lb;
+    private Label m_checkbox_value_lb;
+    private int m_clicks;
+
+    public override LocalizeStringPair Caption
     {
-        private Button m_button;
-        private CheckBox m_checkbox;
-        private Label m_button_lb;
-        private Label m_checkbox_lb;
-        private Label m_button_clicks_lb;
-        private Label m_checkbox_value_lb;
-        private int m_clicks;
+      get { return m_caption; }
+    }
 
-        ///<summary>
-        /// EnglishCaption is the english caption.
-        ///</summary>
-        public override string EnglishCaption { get { return "Section one"; } }
-        ///<summary>
-        /// LocalCaption is the localized caption to display in the section header.
-        ///</summary>
-        public override string LocalCaption { get { return "Section one"; } }
+    ///<summary>
+    /// The Heigth of the section
+    ///</summary>
+    public override int SectionHeight
+    {
+      get
+      {
+        return this.Content.Height;
+      }
+    }
 
-        ///<summary>
-        /// The Heigth of the section
-        ///</summary>
-        public override int SectionHeight
-        {
-            get
-            {
-                return this.Content.Height;
-            }
-        }
+    ///<summary>
+    /// Constructor for SectionOne
+    ///</summary>
+    public SectionOne()
+    {
+      m_caption = new LocalizeStringPair("Section1", "Section1");
+      InitializeComponents();
+      InitializeLayout();
+      RegisterControlEvents();
+    }
 
-        ///<summary>
-        /// Constructor for SectionOne
-        ///</summary>
-        public SectionOne()
-        {
-            InitializeComponents();
-            InitializeLayout();
-            RegisterControlEvents();
-        }
+    private void InitializeComponents()
+    {
+      m_clicks = 0;
 
-        private void InitializeComponents()
-        {
-            m_clicks = 0;
-           
-            m_button = new Button()
-            {
-                Text = "Button1"
-            };
+      m_button = new Button()
+      {
+        Text = "Button1"
+      };
 
-            m_checkbox = new CheckBox();
+      m_checkbox = new CheckBox();
 
-            m_button_lb = new Label()
-            {
-                Text = "",
-                VerticalAlignment = VerticalAlignment.Center,
-                Width = 20
-            };
+      m_button_lb = new Label()
+      {
+        Text = "",
+        VerticalAlignment = VerticalAlignment.Center,
+        Width = 20
+      };
 
-            m_checkbox_lb = new Label()
-            {
-                Text = "",
-                VerticalAlignment = VerticalAlignment.Center,
-                Width = 20
-            };
+      m_checkbox_lb = new Label()
+      {
+        Text = "",
+        VerticalAlignment = VerticalAlignment.Center,
+        Width = 20
+      };
 
-            m_button_clicks_lb = new Label()
-            {
-                Text = "Number of clicks: ",
-                VerticalAlignment = VerticalAlignment.Center,
-            };
+      m_button_clicks_lb = new Label()
+      {
+        Text = "Number of clicks: ",
+        VerticalAlignment = VerticalAlignment.Center,
+      };
 
-            m_checkbox_value_lb = new Label()
-            {
-                Text = "Value of checkbox",
-                VerticalAlignment = VerticalAlignment.Center,
-            };
+      m_checkbox_value_lb = new Label()
+      {
+        Text = "Value of checkbox",
+        VerticalAlignment = VerticalAlignment.Center,
+      };
 
-            m_checkbox_lb.Text = m_checkbox.Checked.ToString();
-            m_button_lb.Text = m_clicks.ToString();
-        }
+      m_checkbox_lb.Text = m_checkbox.Checked.ToString();
+      m_button_lb.Text = m_clicks.ToString();
+    }
 
 
-        private void InitializeLayout()
-        {
-            TableLayout layout = new TableLayout()
-            {
-                // Padding around the table
-                Padding = 10,
-                // Spacing between table cells
-                Spacing = new Eto.Drawing.Size(15, 15),
-                Rows =
+    private void InitializeLayout()
+    {
+      TableLayout layout = new TableLayout()
+      {
+        // Padding around the table
+        Padding = 10,
+        // Spacing between table cells
+        Spacing = new Eto.Drawing.Size(15, 15),
+        Rows =
                 {
                     new TableRow(m_button_clicks_lb, m_button_lb, m_button),
                     new TableRow(m_checkbox_value_lb, m_checkbox, m_checkbox_lb),
                 }
-            };
-            Content = layout;
-        }
-
-        private void RegisterControlEvents()
-        {
-            m_checkbox.CheckedChanged += OnCheckedChanged;
-            m_button.Click += OnButtonClick;
-        }
-
-        private void UnRegisterControlEvents()
-        {
-            m_checkbox.MouseDoubleClick -= OnCheckedChanged;
-            m_button.Click -= OnButtonClick;
-        }
-
-        private void OnCheckedChanged(object sender, EventArgs e)
-        {
-            m_checkbox_lb.Text = m_checkbox.Checked.ToString();
-        }
-
-        private void OnButtonClick(object sender, EventArgs e)
-        {
-            m_clicks++;
-            m_button_lb.Text = m_clicks.ToString();
-        }
-
+      };
+      Content = layout;
     }
+
+    private void RegisterControlEvents()
+    {
+      m_checkbox.CheckedChanged += OnCheckedChanged;
+      m_button.Click += OnButtonClick;
+    }
+
+    private void UnRegisterControlEvents()
+    {
+      m_checkbox.MouseDoubleClick -= OnCheckedChanged;
+      m_button.Click -= OnButtonClick;
+    }
+
+    private void OnCheckedChanged(object sender, EventArgs e)
+    {
+      m_checkbox_lb.Text = m_checkbox.Checked.ToString();
+    }
+
+    private void OnButtonClick(object sender, EventArgs e)
+    {
+      m_clicks++;
+      m_button_lb.Text = m_clicks.ToString();
+    }
+
+  }
 }
